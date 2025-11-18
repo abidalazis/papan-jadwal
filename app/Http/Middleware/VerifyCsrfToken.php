de449b21ce793a2a98542879b0722cda42976d4c
abidalazis <115638163+abidalazis@users.noreply.github.com>
2025-11-18 17:26:51 +0700

    fix csrf

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        //
+    ];
+    
+    /**
+     * Handle an exception.
+     */
+    protected function tokensMatch($request)
+    {
+        $token = $this->getTokenFromRequest($request);
+
+        return is_string($request->session()->token()) &&
+               is_string($token) &&
+               hash_equals($request->session()->token(), $token);
+    }
+}
