28d7aee375eb71148dff40b952b30a5cf0728a07
abidalazis <115638163+abidalazis@users.noreply.github.com>
2025-11-19 14:41:34 +0700

    fix notif email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,14 +7,9 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-        \App\Console\Commands\SendReminderNotification::class,
-    ];
-
     protected function schedule(Schedule $schedule): void
     {
-        // Kirim pengingat setiap hari jam 08:00
-        $schedule->command('reminder:undangan')->everyMinute();
+        $schedule->command('reminder:undangan')->dailyAt('08:00');
     }
 
     protected function commands(): void
